1728f837d7c41e7fd1a92463f90d16243a5fc7f8
Bruna Knopf <brunalaisknopf@gmail.com>
2025-03-28 19:48:13 -0300

    Add files via upload

---
+++ Aula 3 Dados Eco.R
@@ -0,0 +1,9 @@
+# VAMOS FAZER UM DOCUMENTO PUBLICÁVEL (PÁG. DE INT.)
+# PACOTE QUARTO
+
+# install.packages("tidyverse")
+# install.packages("palmerpenguins")
+
+#install.packages("quarto")
+# INSTALEI O QUARTO_
+# PRA EXECUTAR A PÁGINA QUARTO: CNTRL + SHIFT + K
